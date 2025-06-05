430d842581435f0db23465450a045c015fb29a2b
29sebas <segurosebastian29@hotmail.com>
2025-06-05 15:24:09 -0500

    creacion de proyecto parimpar

---
+++ Conceptosbasicosprogramacion/numeroparimpar/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-tem plate for more information
+using System.Diagnostics;
+
+Console.WriteLine("Ingrese un numero:");
+var numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.Write("El numero es par");
+
+}
+else
+{
+    Console.Write("El numero es impar");
+
+
+}
