27c46c4b02369278b1731a66dbc9ff024325a98f
29sebas <segurosebastian29@hotmail.com>
2025-06-12 16:06:35 -0500

    creacion proyecto multiplo

---
+++ Conceptosbasicosprogramacion/multiplo/Program.cs
@@ -0,0 +1,58 @@
+﻿do
+{
+    Console.WriteLine("ingrese primer numero entero");
+    var N1 = (Console.ReadLine());
+
+    if (int.TryParse(N1, out int N1Int))
+    {
+        Console.Write("ingrese segundo numero entero:");
+        var N2 = (Console.ReadLine());
+        if (int.TryParse(N2, out int N2Int))
+        {
+            if (N2Int % N2Int == 0)
+            {
+                Console.WriteLine($"el numero: {N2}, es multiplo de:{N1} ");
+            }
+            else
+            {
+                Console.WriteLine($"el numero: {N2}, no es multiplo de:{N1} ");
+            }
+        } else
+        {
+            Console.WriteLine("debes ingresar un numero entero");
+        }
+    }        
+    else
+    {
+        Console.WriteLine("debes ingresar un numero entero");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+} while (true);
+Console.WriteLine("ingrese primer numero entero");
+
+
+
+
---
+++ Conceptosbasicosprogramacion/numeromayor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Design;
+
+do
+{
+    Console.WriteLine("ingrese primer numero entero:");
+    var N1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese segundo numero entero:");
+    var N2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese tercer numero entero:");
+    var N3 = int.Parse(Console.ReadLine());
+
+    if (N1 > N2 & N2 > N3)
+    {
+        Console.WriteLine($"el numero mayor es:{N1}");
+    }
+    else if (N1 > N3 & N3 > N2)
+    {
+        Console.WriteLine($"el numero mayor es:{N1}");
+    }
+    else if (N2 > N1 & N1 > N3)
+    {
+        Console.WriteLine($"el numero mayor es:{N2}");
+    }
+    else if (N2 > N3 & N3 > N1)
+    {
+        Console.WriteLine($"el numero mayor es:{N2}");
+    }
+    else if (N3 > N1 & N1 > N3)
+    {
+        Console.WriteLine($"el numero mayor es:{N3}");
+
+    }
+
+    else
+    {
+        Console.WriteLine($"el numero mayor es:{N3}");
+    }
+
+
+
+
+
+
+
+    } while (true);
